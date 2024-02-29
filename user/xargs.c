6a14d25784210424d22a4f145d092c5df97b457b
Nickel-Angel <1239004072@qq.com>
2024-02-29 20:34:41 +0800

    fix xargs.c

--- user/xargs.c
+++ user/xargs.c
@@ -23,8 +23,8 @@ xgets(char *buf)
 
 int main(int argc, char *argv[])
 {
-    char buf[512], *args[MAXARG + 1];
-    int len;
+    char buf[512], *args[MAXARG + 1], *p;
+    int len, argsc;
 
     if (argc > MAXARG){
         printf("too many arguments\n");
@@ -35,13 +35,19 @@ int main(int argc, char *argv[])
         args[i - 1] = argv[i];
     }
     while ((len = strlen(xgets(buf))) != 0){
-        args[argc - 1] = buf;
-        args[argc] = 0;
-        // for (int i = 0; i <= argc; ++i){
-        //     printf("%s ", args[i]);
-        // }
-        // printf("\n");
-        // printf("total exec: %s", args[argc - 1]);
+        argsc = argc - 1;
+        p = buf;
+        
+        for (int i = 0; i < len; ++i){
+            if (buf[i] == ' ' || i == len - 1) {
+                buf[i] = '\0';
+                args[argsc] = p;
+                ++argsc;
+                p = buf + i + 1;
+            }
+        }
+        args[argsc] = 0;
+
         if (fork() == 0) {
             exec(argv[1], args);
             exit(0);
