ab1b16caf532289869452d0cc9c29d86255b1fe2
Nickel-Angel <1239004072@qq.com>
2024-03-14 08:35:48 +0800

    add vmprint in vm.c

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmlevelprint(pagetable_t pagetable, int level)
+{
+  if(level > 3)
+    return;
+  for(int i = 0; i < 512; ++i){
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V){
+        uint64 child = PTE2PA(pte);
+
+        for(int i = 0; i < level; ++i)
+          printf(" ..");
+        printf("%d: pte %p pa %p\n", i, pte, child);
+        
+        vmlevelprint((pagetable_t)child, level + 1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmlevelprint(pagetable, 1);
+}
