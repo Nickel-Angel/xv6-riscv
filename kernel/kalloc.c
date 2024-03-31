eb0a2d3eef6cf0dc9408a43cbe85d45f8558e225
Nickel-Angel <1239004072@qq.com>
2024-04-01 00:31:05 +0800

    update kalloc

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,13 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; ++i)
+    initlock(&kmem[i].lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -47,6 +48,11 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  int which_cpu;
+
+  push_off();
+  which_cpu = cpuid();
+  pop_off();
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -56,10 +62,10 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem[which_cpu].lock);
+  r->next = kmem[which_cpu].freelist;
+  kmem[which_cpu].freelist = r;
+  release(&kmem[which_cpu].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,12 +75,31 @@ void *
 kalloc(void)
 {
   struct run *r;
+  int which_cpu;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  which_cpu = cpuid();
+  pop_off();
+
+  acquire(&kmem[which_cpu].lock);
+  r = kmem[which_cpu].freelist;
+  if(r == 0) {
+    for(int i = 0; i < NCPU; ++i)
+    {
+      if(i == which_cpu)
+        continue;
+      acquire(&kmem[i].lock);
+      r = kmem[i].freelist;
+      if(r)
+        kmem[i].freelist = r->next;
+      release(&kmem[i].lock);
+      if(r)
+        break;
+    }
+  } 
+  else
+    kmem[which_cpu].freelist = r->next;
+  release(&kmem[which_cpu].lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
